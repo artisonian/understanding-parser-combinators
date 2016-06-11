6313bf64ddc28e224c704202faa166af9b997c92
Leroy Campbell <leroy@artisonian.com>
2016-06-10 21:59:08 -0400

    Add `andThen` combinator

--- src/Parser.elm
+++ src/Parser.elm
@@ -1,18 +1,33 @@
 module Parser exposing (..)
 
+import Result
 import String
 
 
 type Parser a =
-  Parser (String -> Result String a)
+  Parser (String -> Result String (a, String))
 
 
-parse : Parser a -> String -> Result String a
+parse : Parser a -> String -> Result String (a, String)
 parse (Parser doParse) input =
   doParse input
 
 
-pchar : Char -> Parser (Char, String)
+andThen : Parser a -> Parser b -> Parser (a, b)
+andThen (Parser doFirst) (Parser doRest) =
+  Parser <| \str ->
+    doFirst str
+      `Result.andThen` \(result, rest) ->
+
+    case doRest rest of
+      Err msg ->
+        Err msg
+
+      Ok (result', rest') ->
+        Ok ((result, result'), rest')
+
+
+pchar : Char -> Parser Char
 pchar charToMatch =
   Parser <| \str ->
     case String.uncons str of
